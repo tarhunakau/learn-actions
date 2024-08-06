0228326e733080e5df1d7ec58b3fd2942efc211c
tarhunakau <tarhunakau@users.noreply.github.com>
2024-08-06 13:10:23 +0000

    Tokens updated

--- build/json-nested/types.ts
+++ build/json-nested/types.ts
@@ -1,55 +1,95 @@
 interface IRootObject {
     color: IColor;
     font: IFont;
-    'collection 1': {
-        headerbig: number;
-    };
     typography: ITypography;
 }
 interface IColor {
-    fuschia: IFuschia;
-    iris: IIris;
+    grey: IGrey;
+    red: IRed;
+    green: IGreen;
+    orange: IOrange;
+    blue: IBlue;
+    purple: IPurple;
+    brand: IBrand;
 }
-interface IFuschia {
-    60: string;
+interface IGrey {
+    4: string;
+    5: string;
+    10: string;
+    15: string;
+    24: string;
+    40: string;
+    50: string;
     80: string;
-    100: string;
+    90: string;
+    'light grey': string;
+    '2 5': string;
 }
-interface IIris {
+interface IRed {
+    5: string;
+    10: string;
+    20: string;
     60: string;
-    80: string;
-    100: string;
+    pressed: string;
+    hover: string;
+    'default': string;
+}
+interface IGreen {
+    5: string;
+    10: string;
+    20: string;
+    60: string;
+    pressed: string;
+    hover: string;
+    'default': string;
+}
+interface IOrange {
+    5: string;
+    10: string;
+    20: string;
+    60: string;
+    pressed: string;
+    hover: string;
+    'default': string;
+}
+interface IBlue {
+    5: string;
+    10: string;
+    20: string;
+    60: string;
+    pressed: string;
+    hover: string;
+    'default': string;
+}
+interface IPurple {
+    5: string;
+    10: string;
+    20: string;
+    60: string;
+    pressed: string;
+    hovered: string;
+    'default': string;
+}
+interface IBrand {
+    'dark grey': string;
+    'smart red': string;
+    'grey 30': string;
+    white: string;
 }
 interface IFont {
-    'header 1': {
-        fontSize: number;
-        textDecoration: string;
-        fontFamily: string;
-        fontWeight: number;
-        fontStyle: string;
-        fontStretch: string;
-        letterSpacing: number;
-        lineHeight: number;
-        paragraphIndent: number;
-        paragraphSpacing: number;
-        textCase: string;
-    };
-    'header 2': {
-        fontSize: number;
-        textDecoration: string;
-        fontFamily: string;
-        fontWeight: number;
-        fontStyle: string;
-        fontStretch: string;
-        letterSpacing: number;
-        lineHeight: number;
-        paragraphIndent: number;
-        paragraphSpacing: number;
-        textCase: string;
-    };
+    header: IHeader;
+    subtitle: ISubtitle;
     body: IBody;
+    buttons: IButtons;
 }
-interface IBody {
+interface IHeader {
+    xxlarge: IXxlarge;
+    xlarge: IXlarge;
+    large: ILarge;
+    medium: IMedium;
+    base: IBase;
+}
+interface IXxlarge {
     fontSize: number;
     textDecoration: string;
     fontFamily: string;
@@ -62,33 +102,91 @@ interface IBody {
     paragraphSpacing: number;
     textCase: string;
 }
+interface IXlarge {
+    fontSize: number;
+    textDecoration: string;
+    fontFamily: string;
+    fontWeight: number;
+    fontStyle: string;
+    fontStretch: string;
+    letterSpacing: number;
+    lineHeight: number;
+    paragraphIndent: number;
+    paragraphSpacing: number;
+    textCase: string;
+}
+interface ILarge {
+    fontSize: number;
+    textDecoration: string;
+    fontFamily: string;
+    fontWeight: number;
+    fontStyle: string;
+    fontStretch: string;
+    letterSpacing: number;
+    lineHeight: number;
+    paragraphIndent: number;
+    paragraphSpacing: number;
+    textCase: string;
+}
+interface IMedium {
+    fontSize: number;
+    textDecoration: string;
+    fontFamily: string;
+    fontWeight: number;
+    fontStyle: string;
+    fontStretch: string;
+    letterSpacing: number;
+    lineHeight: number;
+    paragraphIndent: number;
+    paragraphSpacing: number;
+    textCase: string;
+}
+interface IBase {
+    fontSize: number;
+    textDecoration: string;
+    fontFamily: string;
+    fontWeight: number;
+    fontStyle: string;
+    fontStretch: string;
+    letterSpacing: number;
+    lineHeight: number;
+    paragraphIndent: number;
+    paragraphSpacing: number;
+    textCase: string;
+}
+interface ISubtitle {
+    large: ILarge;
+    medium: IMedium;
+    base: IBase;
+    small: ISmall;
+}
+interface ISmall {
+    fontSize: number;
+    textDecoration: string;
+    fontFamily: string;
+    fontWeight: number;
+    fontStyle: string;
+    fontStretch: string;
+    letterSpacing: number;
+    lineHeight: number;
+    paragraphIndent: number;
+    paragraphSpacing: number;
+    textCase: string;
+}
+interface IBody {
+    medium: IMedium;
+    base: IBase;
+    small: ISmall;
+}
+interface IButtons {
+    medium: IMedium;
+    base: IBase;
+    small: ISmall;
+}
 interface ITypography {
-    'header 1': {
-        fontSize: number;
-        textDecoration: string;
-        fontFamily: string;
-        fontWeight: number;
-        fontStyle: string;
-        fontStretch: string;
-        letterSpacing: number;
-        lineHeight: number;
-        paragraphIndent: number;
-        paragraphSpacing: number;
-        textCase: string;
-    };
-    'header 2': {
-        fontSize: number;
-        textDecoration: string;
-        fontFamily: string;
-        fontWeight: number;
-        fontStyle: string;
-        fontStretch: string;
-        letterSpacing: number;
-        lineHeight: number;
-        paragraphIndent: number;
-        paragraphSpacing: number;
-        textCase: string;
-    };
+    header: IHeader;
+    subtitle: ISubtitle;
     body: IBody;
+    buttons: IButtons;
 }
 
